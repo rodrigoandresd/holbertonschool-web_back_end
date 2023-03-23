11514c5d596339dd29b43813754bde60368b6645
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-23 16:36:11 -0500

    task 3 all

---
+++ ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    })
+    .catch((error) => {
+      console.log('');
+      console.error(error)
+    });  
+}
+
