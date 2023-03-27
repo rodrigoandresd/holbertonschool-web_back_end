0b3442bcb2f34eb5ece55a5ccc6403a22d1fa473
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-27 17:10:31 -0500

    task 8 clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -4,7 +4,11 @@ export default function cleanSet(set, startString) {
 
   set.forEach((value) => {
     if (value.startsWith(startString)) {
-      filteredValues.push(value.substring(startString.length));
+      const restOfString = value.substring(startString.length);
+      const nextCharIndex = restOfString.indexOf('-') + 1;
+      if (nextCharIndex && restOfString.charCodeAt(nextCharIndex) >= 65 && restOfString.charCodeAt(nextCharIndex) <= 90 || restOfString.charCodeAt(nextCharIndex) >= 97 && restOfString.charCodeAt(nextCharIndex) <= 122) {
+        filteredValues.push(restOfString.substring(nextCharIndex));
+      }
     }
   });
 
