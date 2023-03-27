064239f849b3f69af96d0a6b96f60c5e21ca26c4
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-27 13:31:23 -0500

    task 4 update grade

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,13 @@
+function updateStudentGradeByCity(listStudents, city, newGrades) {
+  return listStudents
+    .filter((listStudents) => listStudents.location === city)
+    .map((listStudents) => {
+      const newGrade = newGrades.find((grade) => grade.studentId === listStudents.id);
+      return {
+        ...listStudents,
+        grade: newGrade ? newGrade.grade : "N/A",
+      };
+    });
+}
+
+export default updateStudentGradeByCity;
