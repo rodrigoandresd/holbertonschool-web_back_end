d7bdbc9454a3644d8e5f14f344adbcb480311de7
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-27 11:30:25 -0500

    task 3 ged ads sum

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,6 +1,6 @@
 export default function getStudentIdsSum(listStu) {
   if (Array.isArray(listStu)) {
     return listStu.reduce((accumulator, currentStud) => accumulator + currentStud.id, 0);
-  } 
+  }
   return [];
 }
