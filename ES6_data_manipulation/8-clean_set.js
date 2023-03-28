be6791b47fdb5b4ed06a96c1add4251cc664ab24
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-03-28 10:20:02 -0500

    task 8-

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,17 +1,12 @@
-export default function cleanSet(set, startString) {
-  if (!startString || !startString.length) return '';
-
-  let filteredValues = '';
-  const uniqueValues = new Set();
-  set.forEach((value) => {
-    if (value.startsWith(startString)) {
-      const filteredValue = value.substring(startString.length);
-      if (!uniqueValues.has(filteredValue)) {
-        filteredValues += (filteredValues.length > 0 ? '-' : '') + filteredValue;
-        uniqueValues.add(filteredValue);
-      }
+export default function cleanSet(set, string) {
+  if (!string || !string.length) return '';
+  let value = '';
+  for (const element of set) {
+    if (element && element.startsWith(string)) {
+      value += value.length === 0 ? element.replace(string, '') : element.replace(string, '-');
     }
-  });
+  }
+  return value;
+};
 
-  return filteredValues;
-}
+  
