00e931d22f4c84bfbb1be7ba3478259df5812c9a
rodrigoandresd <rodrigoaduqueg@gmail.com>
2023-08-03 16:11:16 -0500

    100-weak

--- ES6_data_manipulation/100-weak.js
+++ ES6_data_manipulation/100-weak.js
@@ -6,6 +6,6 @@ export function queryAPI(endpoint) {
   }
   weakMap.set(endpoint, weakMap.get(endpoint) + 1);
   if (weakMap.get(endpoint) >= 5) {
-    throw new Error("Endpoint load is high");
+    throw new Error('Endpoint load is high');
   }
 }
